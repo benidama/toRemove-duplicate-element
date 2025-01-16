51b3c7fe5c2e5f5230c542b23bfa1541565c26ec
benidama <benidama02@gmail.com>
2025-01-16 20:24:06 +0000

    Adding day challenge

--- remove.js
+++ remove.js
@@ -11,3 +11,35 @@ function removeDuplicate() {
   return result;
 }
 removeDuplicate();
+
+// function logicalCalc(array, op){
+//   //your code here
+//   let arr = array[0];
+
+// for(let i = 1; i<array.length; i++){
+//   if(op == "AND"){
+//     arr = arr && array[i]
+//   }else if (op == "OR"){
+//     arr == arr || array[i]
+//   }else{
+//     arr = arr != array[i]
+//   }
+// }
+//   return arr;
+// }
+//Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+function logicalCalc(array, op) {
+  var result = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (op == "AND") {
+      result = result && array[i];
+    }
+    if (op == "OR") {
+      result = result || array[i];
+    }
+    if (op == "XOR") {
+      result = result != array[i];
+    }
+  }
+  return result;
+}
